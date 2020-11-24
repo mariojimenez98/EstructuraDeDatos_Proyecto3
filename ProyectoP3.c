2fbaff74c8474380ae9e83b75ef98846a91519fd
Mario Jimenez <50642172+mariojimenez98@users.noreply.github.com>
2020-11-24 11:05:26 -0600

    Add files via upload

---
+++ ProyectoP3.c
@@ -0,0 +1,18 @@
+/********************************************
+    Nombre de Alumnos: Mario Ivan Jimenez Alfaro    #727740
+                       Benjamin Gil Flores          #727211
+                       Juan Enrique Cibrian Loera   #725290
+                       Alejandro Sada Prendes       #725958
+                       Carlos Manuel Alanis Pe�a    #732158
+    Fecha: 11 de Diciembre 2020
+    Materia: Estructura de Datos
+    Programa: Proyecto 3
+    Descripcion:
+********************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    printf("Do something");
+}
